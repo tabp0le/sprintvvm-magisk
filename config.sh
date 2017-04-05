db2b3a82115ddecc5b6674d49767778c75e29b33
Tab Fitts <tfitts1212@gmail.com>
2017-04-05 15:48:58 -0700

    Initial Sprint VVM commit

--- config.sh
+++ config.sh
@@ -27,7 +27,7 @@
 
 # This will be the folder name under /magisk
 # This should also be the same as the id in your module.prop to prevent confusion
-MODID=template
+MODID=sprintvvm
 
 # Set to true if you need to enable Magic Mount
 # Most mods would like it to be enabled
@@ -50,7 +50,8 @@ LATESTARTSERVICE=false
 
 print_modname() {
   ui_print "*******************************"
-  ui_print "     Magisk Module Template    "
+  ui_print "     Sprint VVM Magisk Module  "
+  ui_print "           by tabp0le          "
   ui_print "*******************************"
 }
 
